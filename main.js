576ae7e60e27285a29ab88083d8014b40b7fef66
beet <63141491+beetcb@users.noreply.github.com>
2020-12-18 17:23:28 +0800

    feat: store cookie, login in only when its needed

--- main.js
+++ main.js
@@ -8,17 +8,46 @@ const { signApp } = require('./campusphere/app')
 const school = conf.get('school')
 const users = conf.get('users')
 log.object(users)
-if (!users.length) log.error('未找到用户,请运行 ./init.js -u 配置')
-users.forEach(async i => {
-  /**
-   * Keys of this cookie Object:
-   * YOU CAN USE THIS COOKIE FOR EVERYTHING
-   * @compusphere something about cp daliy's app
-   * @swms continuing log into your school's swms [stu work magagement system]
-   */
-  const cookie = await login(school, i)
+if (!users) {
+  log.error('未找到用户,请运行 ./init.js -u 配置')
+  process.exit(1)
+}
 
-  const sign = new signApp(school, cookie)
-  await sign.signInfo()
+/**
+ * Keys of this cookie Object:
+ * YOU CAN USE THIS COOKIE FOR EVERYTHING
+ * @compusphere something about cp daliy's app
+ * @swms continuing log into your school's swms [stu work magagement system]
+ */
+let cookie, storeCookiePath
+
+users.forEach(async i => {
+  storeCookiePath = `cookie.${i.alias || i.username}`
+
+  if (!conf.get(storeCookiePath)) {
+    await reLogin(i)
+  } else {
+    storeCookie(storeCookiePath)
+  }
+
+  let sign = new signApp(school, cookie, i)
+  const isNeedLogIn = await sign.signInfo()
+  if (isNeedLogIn) {
+    await reLogin(i)
+    sign = new signApp(school, cookie)
+    await sign.signInfo()
+  }
   await sign.signWithForm()
+
+  process.exit(0)
 })
+
+async function reLogin(i) {
+  cookie = await login(school, i)
+  conf.set(storeCookiePath, cookie)
+}
+
+function storeCookie(path) {
+  cookie = conf.get(path)
+  log.success('Using stored cookie')
+}
