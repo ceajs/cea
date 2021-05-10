1c800956f7090e2de805b3df709c69ad83c68343
beetcb <63141491+beetcb@users.noreply.github.com>
2021-05-10 21:57:28 +0800

    feat(conf): add types for users config

---
+++ types/conf.ts
@@ -0,0 +1,11 @@
+export type UsersConf = {
+  users: Array<UserConfOpts>
+}
+
+export type UserConfOpts = {
+  username: string
+  password: string
+  alias?: string
+  school: string
+  addr: string | Array<string>
+}
