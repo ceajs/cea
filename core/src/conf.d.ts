7d898b20ce7938a8070faccdec196201223e460f
beetcb <63141491+beetcb@users.noreply.github.com>
2021-05-24 21:13:22 +0800

    chore: clean up

--- core/src/conf.d.ts
+++
@@ -1,3 +0,0 @@
-import { UsersConf, SchoolConf } from './types/conf';
-export declare function loadConfFromToml(): UsersConf | null;
-export declare function getSchoolInfos(users: UsersConf): Promise<SchoolConf | null>;
--- core/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-import log from './utils/logger';
-export declare const sstore: any;
-export * from './types/conf';
-export * from './types/cookie';
-export * from './types/helper';
-export * from './conf';
-export { log };
-export declare function handleCookie(): Promise<void>;
