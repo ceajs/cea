e8324227445b815f74b8488cdc166cefb64a4cbd
beetcb <63141491+beetcb@users.noreply.github.com>
2021-12-05 23:17:59 +0800

    build: remove the default configured attendance

--- serverless/cea-tcb/index.js
+++ serverless/cea-tcb/index.js
@@ -3,9 +3,10 @@ exports.main = () => {
   const { default: Cea, checkIn, attendanceCheckIn } = require('../cea-cjs/lib/src/index')
   // 创建 Cea 的实例
   const cea = new Cea()
-  // 注册插件
+  // 注册签到插件
   cea.addPlugin(checkIn)
-  cea.addPlugin(attendanceCheckIn)
+  // 注册查寝插件，如需开启，请移除下行注释
+  // cea.addPlugin(attendanceCheckIn)
   // 执行签到脚本
   cea.start()
 }
