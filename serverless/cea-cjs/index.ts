b3d7763cd0a9c6e1b816d8e8f9779b170ace4141
beetcb <63141491+beetcb@users.noreply.github.com>
2021-12-13 23:43:48 +0800

    chore: bump cea version for cjs

--- serverless/cea-cjs/index.ts
+++ serverless/cea-cjs/index.ts
@@ -1,3 +1,3 @@
-import Cea, { attendanceCheckIn, checkIn } from '../../src/internal'
+import Cea, { attendanceCheckIn, checkIn } from '../../src/internal/lib/src/index'
 export default Cea
 export { attendanceCheckIn, checkIn }
